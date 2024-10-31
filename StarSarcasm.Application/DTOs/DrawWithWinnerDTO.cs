f3cccf16a559937336610e6fde23b62902c9d63a
mohdali300 <mohdali30060@gmail.com>
2024-10-31 19:39:29 +0300

    Edit Last Draws & Last 4 Draws

---
+++ StarSarcasm.Application/DTOs/DrawWithWinnerDTO.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarSarcasm.Application.DTOs
+{
+	public class DrawWithWinnerDTO
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string? Description { get; set; }
+		public DateTime StartAt { get; set; }
+		public DateTime EndAt { get; set; }
+		public string? ImagePath { get; set; }
+		public int SubscribersNumber { get; set; }
+		public bool IsActive { get; set; }
+
+		public WinnerDTO? Winner { get; set; }
+	}
+
+	public class WinnerDTO
+	{
+		public string UserId { get; set; }
+		public string UserName { get; set; }
+		public string Email { get; set; }
+		public DateTime BirthDate { get; set; }
+		public DateTime? LastWinDate { get; set; }
+		public bool IsSubscribed { get; set; }
+
+
+	}
+}
